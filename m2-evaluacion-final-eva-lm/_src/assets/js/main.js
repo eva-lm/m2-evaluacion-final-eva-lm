a11db5ed6640dfc090f1d663d6d319e9bcd2de3f
eva-lm <eva.lm1810@gmail.com>
2019-08-09 20:13:06 +0200

    add listen function add favorite list git push

--- m2-evaluacion-final-eva-lm/_src/assets/js/main.js
+++ m2-evaluacion-final-eva-lm/_src/assets/js/main.js
@@ -3,18 +3,18 @@
 const btn = document.querySelector(".js-button");
 const searchInput = document.querySelector(".js-search");
 //let shows = [];
-let result = [];
+let favorites = [];
+let series = [];
 
 const getDataFromServer = () => {
- result = [];
+ series = [];
   return fetch(`http://api.tvmaze.com/search/shows?q=${searchInput.value}`)
     .then(response => response.json())
     .then(data => {
       formatData(data);
-      //saveData(data);
       paintShows();
       //console.log(data);
-      // listenPalettes();
+      listenShows();
       // setPalettesIntoLocalStorage();
     });
 };
@@ -24,14 +24,16 @@ const formatData = function(data) {
   //console.log(data);
   for (const item of data) {
     if (item.show.image === null) {
-      result.push({
+      series.push({
         name: item.show.name,
-        image:"https://via.placeholder.com/210x295/ffffff/666666/?text=TV"
+        image:"https://via.placeholder.com/210x295/ffffff/666666/?text=TV",
+        id: item.show.id
       });
     } else {
-    result.push({
+    series.push({
       name: item.show.name,
-      image: item.show.image.medium
+      image: item.show.image.medium,
+      id: item.show.id
     });
   }
   }
@@ -39,73 +41,68 @@ const formatData = function(data) {
   /*   console.log(
     "Guardamos en una array las series con los valores de titulo e imagen"
   ); */
-  console.log(result);
-  return result;
 };
-
-/* const saveData = function(data) {
-  result = data;
-}; */
 
 //PINTO
 
 const paintShows = function() {
   const jsUl = document.querySelector(".js-ul");
-  jsUl.innerHTML = "";
-  for (let serieIndex = 0; serieIndex < result.length; serieIndex++) {
-    jsUl.innerHTML += `<li class="js-li">${result[serieIndex].name}</li><li class="js-li"><img src=${result[serieIndex].image}></li>`;
+  let text = "";
+  for (let serieIndex = 0; serieIndex < series.length; serieIndex++) {
+    text += `<li class="js-li" data-index="${serieIndex}">
+      ${series[serieIndex].name}
+      <img src="${series[serieIndex].image}">
+    </li>`;
   }
- 
+ jsUl.innerHTML = text;
+ console.log(jsUl);
 };
 
 //ESCUCHO
-const handleClick = function() {
-  console.log(
-    "generamos la función a partir del click anterior, o sea la metemos en favoritos o no"
-  );
-  const serieIndex = getClickedShows(ev);
-  if (isFavoriteShow(serieIndex)) {
-      removeFavorite(serieIndex);
-  } else {
-      addFavorite(serieIndex);
-  }
-  paintShows();
-  listenShows();
-};
 
-const listenShows = function() {
-  console.log("Escuchamos en donde hacemos click");
-  const serieElements = document.querySelectorAll(".js-li");
-    for (const serieElement of serieElements) {
-        serieElement.addEventListener("click", handleClick);
-    }
+function listenShows() {
+  const seriesList = document.querySelectorAll(".js-li");
+  console.log(seriesList);
+  for (let serieIndex = 0; serieIndex < series.length; serieIndex++) {
+  seriesList[serieIndex].addEventListener("click", getClikedShows);
+}
 };
 
 //LEO
-const getClickedShows = function(ev) {
-  const currentTarget = ev.currentTarget;
-  const clickedSerieIndex = parseInt(currentTarget.dataset.index);
-  console.log("Te dice que serie esta seleccionada");
-  return clickedSerieIndex;
+function getClikedShows(ev) {
+ //Te dice que serie esta seleccionada
+  const index = parseInt(ev.currentTarget.dataset.index);
+  if (isFavoriteShow(index)) {
+    
+    removeFavorite(index)
+    // borro
+  } else {
+   
+    addFavorite(index);
+    // añado
+  }
+  paintShows();
+  listenShows();
+  console.log(favorites);
 };
 
+const isFavoriteShow = function(index) {
+  const serie = series[index];
+  for (const favorite of favorites) {
+    if (favorite.id === serie.id) {
+      return true;
+    }
+  }
+  return false;
+};
 
+function addFavorite(index){
+  favorites.push(series[index]);
+};
 
-const isFavoriteShow = function() {
-  console.log("Leemos si la serie está en favorito o no");
+const removeFavorite = function(index) {
   
+  favorites.splice(favoriteIndex, id);
 };
 
-
-const addFavorite = function() {
-  console.log("Añadimos a la lista favoritos si no está");
-  serieIndex.classList.add("selected");
-
-};
-
-const removeFavorite = function() {
-  console.log("eliminamos de la lista de favoritos");
-  serieIndex.classList.add("unselected");
-
-};
 btn.addEventListener("click", getDataFromServer);
